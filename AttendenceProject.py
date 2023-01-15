3573d425e4753ad952309bab49cc81b345b832db
Suriya T <76655554+Suriya2210@users.noreply.github.com>
2023-01-15 17:03:10 +0530

    Update AttendenceProject.py

--- AttendenceProject.py
+++ AttendenceProject.py
@@ -27,11 +27,10 @@ def findencodings(images):
         encodeList.append(encode)
     return encodeList
 
-def markAttendence(name):
+def markAttendance(name):
     with open('Attendance.csv', 'r+') as f:
         myDatalist = f.readlines()
         nameList=[]
-        # print(myDatalist)
         for line in myDatalist:
             entry = line.split(',')
             nameList.append(entry[0])
@@ -73,7 +72,7 @@ while True:
             cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255.0), 2)
             cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
             cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
-            markAttendence(name)
+            markAttendance(name)
 
     cv2.imshow('Webcam', img)
     cv2.waitKey(1)
